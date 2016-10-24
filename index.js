c9286dec089a0258140f93c293106e33113d3170
selva <selvaraj.antonyraj@sc.com>
2016-10-24 11:04:52 +0800

    Fixes #4

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ module.exports = {
     return stew.mv(bootstrapAssetPath('javascripts'), 'bootstrap');
   },
   included: function(app) {
-    var plugins = (app.options.bootstrap || {}).plugins;
+    var options = app.app.options || app.options || {};
+    var plugins = options.plugins;
     if (Array.isArray(plugins)) {
       plugins.forEach(function(name) {
         app.import('vendor/bootstrap/bootstrap/' + name + '.js');
